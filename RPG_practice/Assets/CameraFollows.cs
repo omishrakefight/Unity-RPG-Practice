48e8a6ebc647bc156eccc35f0aaa459e16a6b3da
Kyle Genter <genter1993@live.com>
2018-06-04 00:21:51 -0400

    character and camera
    inported cameras, stuck with generic follow from above.  Improved Ethans turn speed and move speeds.

---
+++ RPG_practice/Assets/CameraFollows.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollows : MonoBehaviour {
+    /*
+    Vector3 startPosition;
+    Vector3 offset;
+    Vector3 cameraStart;
+    */
+    GameObject player;
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        /*
+        startPosition = player.transform.position;
+        cameraStart = gameObject.transform.position;
+        */
+    }
+
+    private void LateUpdate()
+    {
+        gameObject.transform.position = player.transform.position;
+        /*
+        offset = player.transform.position - startPosition;
+        gameObject.transform.position = cameraStart + offset;
+        */
+    }
+}
